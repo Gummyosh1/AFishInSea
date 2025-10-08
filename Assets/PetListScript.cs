13ec4d845cc4fa3ff30c760c3f4d7e51cefd0ca7
Gummyosh1 <gummyoshi@gmail.com>
2025-10-08 01:10:33 -0400

    Clean
    I bugged it silly ol me

--- Assets/PetListScript.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PetListScript : MonoBehaviour
-{
-    //0 to 2 are left walk, 3 to 5 are right walk, 6 is sleep for cats
-    public Sprite[] petSleep;
-}
